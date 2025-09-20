26d23d41ef4af5a7ed38d8afacc9719a15c16d40
Aki <141536869+AlexAutumns@users.noreply.github.com>
2025-09-20 05:47:26 +0400

    added logo to public/

--- app/root.tsx
+++ app/root.tsx
@@ -38,6 +38,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
                 />
                 <Meta />
                 <Links />
+                <link rel="icon" type="image/png" href="/Logo.png" />
+                <title>Akia Hans - Portfolio</title>
             </head>
             <body>
                 {children}
